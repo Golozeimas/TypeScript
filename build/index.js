f1f82f4a013be2cab6591f9cf867fef33374f0b6
Matheus+ <joaosobramatheus@gmail.com>
2026-05-05 10:22:32 -0300

    initial commit

---
+++ build/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const nome = "matheus";
+console.log("Meu nome é: " + nome);
+console.log("Initial commit");
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+const nome:string = "matheus";
+
+console.log("Meu nome é: " + nome);
+
+console.log("Initial commit")
