8837918e9c46e13b7d3f4476f242b1dc761c23a0
Matheus+ <joaosobramatheus@gmail.com>
2026-05-05 11:34:43 -0300

    study: aprendizado sobre o Type Boolean

---
+++ build/boolean.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let estarVerificado = false;
+const nome = "matheus";
+if ("matheus" === nome) {
+    console.log("Você estar verificado!");
+    estarVerificado = true;
+}
---
+++ src/boolean.ts
@@ -0,0 +1,8 @@
+let estarVerificado:boolean = false
+
+const nome: string = "matheus"
+
+if("matheus" === nome){
+    console.log("Você estar verificado!")
+    estarVerificado = true
+}
