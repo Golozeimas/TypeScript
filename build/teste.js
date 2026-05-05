bde0b14e525b6e8d638be9fc0f17b3dbc2dea729
Matheus+ <joaosobramatheus@gmail.com>
2026-05-05 10:44:14 -0300

    study: tipo de inferência no typescript

---
+++ build/teste.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let nome = "matheus";
+// nome = 10; erro por inferência na inicialização
+console.log(nome);
+let nomes = [
+    "Matheus",
+    "Lucas"
+];
+nomes.push(20); // não podemos por ser um número em um array inicializado como STRING
+console.log(nomes);
---
+++ src/teste.ts
@@ -0,0 +1,17 @@
+let nome = "matheus";
+
+// nome = 10; erro por inferência na inicialização
+
+console.log(nome)
+
+
+let nomes = [
+    "Matheus",
+    "Lucas"
+]
+
+
+nomes.push(20); // não podemos por ser um número em um array inicializado como STRING
+
+console.log(nomes)
+
