7013ec7db29444bed53f943e5c9511ebaa2ecde1
Matheus+ <joaosobramatheus@gmail.com>
2026-05-05 11:30:50 -0300

    study: aprendizado sobre o any no TypeScript

---
+++ build/any.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let valor = 10; // perde o sentido do TypeScript, vira basicamente JS
+valor = { nome: "matheus" };
+valor = "lucas";
+valor = true;
+console.log(valor);
---
+++ src/any.ts
@@ -0,0 +1,10 @@
+let valor: any = 10 // perde o sentido do TypeScript, vira basicamente JS
+
+
+valor = { nome: "matheus"}
+
+valor = "lucas"
+
+valor = true
+
+console.log(valor)
