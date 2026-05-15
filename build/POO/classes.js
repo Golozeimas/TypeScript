35b8fd734565a1cb43f35d4232e41ae475212862
Matheus+ <joaosobramatheus@gmail.com>
2026-05-15 12:05:14 -0300

    study: aprendizado sobre classes no TypeScript

---
+++ build/POO/classes.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Pessoa {
+    // atributos
+    nome;
+    idade;
+    // construtor: ao instância definir valores iniciais para os atributos
+    // inicializa junto com a classe esse método
+    constructor(nome, idade) {
+        // this -> é usado para acessar atributos dentro da classe
+        // instancia a própria classe
+        this.nome = nome;
+        this.idade = idade;
+    }
+    // método que fica dentro da classe, como uma função mas nesse caso n precisa
+    // de nenhuma palavra reservada só o retorno
+    apresentar() {
+        console.log(`Meu nome é ${this.nome}`);
+        console.log(`E eu tenho ${this.idade} anos de idade`);
+    }
+}
+// fora da classe instanciando objetos
+const pessoa1 = new Pessoa("Matheus", 20);
+pessoa1.apresentar();
---
+++ src/POO/classes.ts
@@ -0,0 +1,27 @@
+class Pessoa{
+	// atributos
+	nome: string;
+	idade: number;
+	
+		
+		// construtor: ao instância definir valores iniciais para os atributos
+        // inicializa junto com a classe esse método
+		constructor(nome: string, idade: number){
+			// this -> é usado para acessar atributos dentro da classe
+            // instancia a própria classe
+			this.nome = nome
+			this.idade = idade
+		}
+	
+	// método que fica dentro da classe, como uma função mas nesse caso n precisa
+	// de nenhuma palavra reservada só o retorno
+	apresentar(): void{
+		console.log(`Meu nome é ${this.nome}`)
+        console.log(`E eu tenho ${this.idade} anos de idade`)
+	}
+}
+
+// fora da classe instanciando objetos
+const pessoa1 = new Pessoa("Matheus", 20)
+
+pessoa1.apresentar()
