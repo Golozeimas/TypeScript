9779f2c527de17ad8e2632cc570a99e229d48948
Matheus+ <joaosobramatheus@gmail.com>
2026-05-27 11:37:32 -0300

    study: aprendizado sobre gets e setters no TypeScript

---
+++ src/POO/gets_e_sets.ts
@@ -0,0 +1,41 @@
+class Game{
+    private _nome_do_jogo: string;
+    private _ip_do_jogo
+
+    constructor(_nome_do_jogo: string, _ip_do_jogo: string){
+        this._nome_do_jogo = _nome_do_jogo;
+        this._ip_do_jogo = _ip_do_jogo
+    }
+
+    get nome_do_jogo(): string{
+        return this._nome_do_jogo
+    }
+
+    set nome_do_jogo(valor: string){
+        if(valor.length > 0 && valor !==  undefined){
+            this._nome_do_jogo = valor
+        }
+    }
+
+    get ip_do_jogo(){
+        return this._ip_do_jogo
+    }
+
+    set ip_do_jogo(valor_do_ip: string){
+        if(valor_do_ip.length === 4){
+            this._ip_do_jogo = valor_do_ip
+        }
+    }
+}
+
+const fortnite = new Game("Fortnite", "1234")
+
+console.log(fortnite)
+
+fortnite.nome_do_jogo = "fortnite"
+
+fortnite.ip_do_jogo = "4321"
+
+console.log(fortnite.nome_do_jogo)
+
+console.log(fortnite.ip_do_jogo)
